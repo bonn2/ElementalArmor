37986f8f6f659b83c50cd82f4e3b64858fa3a908
bonn2 <bonifas.fetzer@gmail.com>
2020-09-16 01:47:33 -0400

    Remove Extra Import

--- src/main/java/bonn2/elementalarmor/util/Counter.java
+++ src/main/java/bonn2/elementalarmor/util/Counter.java
@@ -2,7 +2,6 @@ package bonn2.elementalarmor.util;
 
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 public class Counter {
