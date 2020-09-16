e916a15273975b3d3fb5b33754bb86634d11d638
Jaimss <james@harrell365.com>
2020-09-15 21:00:44 -0400

    base explosion setup

---
+++ src/main/java/bonn2/elementalarmor/listeners/fire/Explosion.java
@@ -0,0 +1,16 @@
+package bonn2.elementalarmor.listeners.fire;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
+
+public class Explosion implements Listener {
+
+    @EventHandler
+    public void onDoubleCrouch(PlayerToggleSneakEvent event) {
+
+
+
+    }
+
+}
