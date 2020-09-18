e278c8fa89907c825123e40e3f2bd495e38d7bce
Jaimss <james@harrell365.com>
2020-09-18 11:08:57 -0400

    Create HomePage.java

---
+++ src/main/java/ca/minecore/elementalarmor/gui/pages/HomePage.java
@@ -0,0 +1,17 @@
+package ca.minecore.elementalarmor.gui.pages;
+
+import ca.minecore.elementalarmor.gui.AbstractEnigmaGUI;
+import ca.minecore.elementalarmor.util.ChatUtil;
+import me.mattstudios.mfgui.gui.guis.Gui;
+import org.bukkit.entity.Player;
+
+public class HomePage extends AbstractEnigmaGUI {
+
+    @Override
+    public Gui getGui(Player player) {
+        Gui gui = new Gui(5, ChatUtil.colorize("&6Minecore &7- &bEnigma"));
+
+        return gui;
+    }
+
+}
